07f575d419740f316aeb55f99f8222e6452c7a5c
AFulle <afuller@csumb.edu>
2017-09-15 11:15:28 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,18 +1,19 @@
 // Card Class
+
 public class Card
 {
    enum Suit {clubs, diamonds, hearts, spades};
    private char value;
    private Suit suit;
    private boolean errorFlag;
-   
+
    public Card(char value, Suit suit)
    {
       setValue();
       setSuit();
       setCard(value, suit);
    }
-   
+
    // this method displays the card if valid and Invalid Card otherwise
    public String toString()
    {
@@ -23,11 +24,11 @@ public class Card
       }
       else 
       {
-         
+
       }
       return output;
    }
-   
+
    // mutator that accepts legal values 
    public boolean set(char value, Suit suit)
    {
@@ -35,10 +36,44 @@ public class Card
       this.suit = suit;
       return errorFlag;
    }
-   
+
    // accessor for suit
    public String getSuit()
    {
+
+   }
+
+   public String getValue()
+   {
+
+   }
+
+   public boolean equals(Card card)
+   {
+
+   }
+
+   private boolean isValid(char value, Suit suit)
+   {
+      switch(value)
+      {
+         case 'A':
+         case '2':
+         case '3':
+         case '4':
+         case '5':
+         case '6':
+         case '7':
+         case '8':
+         case '9':
+         case 'T':
+         case 'J':
+         case 'Q':
+         case 'K':
+            return true;
+         default:
+            return false;
+      }
       
    }
 
